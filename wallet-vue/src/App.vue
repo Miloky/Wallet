ab792d0f7069d18f54d4ee640ed2de9ce13fc1b7
miloky <monmorti@gmail.com>
2023-08-24 18:23:47 +0300

    side menu

--- wallet-vue/src/App.vue
+++ wallet-vue/src/App.vue
@@ -1,61 +1,24 @@
 <template>
-  <q-layout view="lHh Lpr lff" container :style="{ height: `${height}px` }">
+  <div v-if="authGuardState.loading">loading...</div>
+  <q-layout
+    v-if="authGuardState.authenticated"
+    view="lHh Lpr lff"
+    container
+    :style="{ height: `${height}px` }"
+  >
     <q-header elevated>
       <q-toolbar>
-        <!-- <WalletIcon class="cursor-pointer" @click="navigateHome" /> -->
         <q-toolbar-title class="non-selectable cursor-pointer" @click="navigateHome"
           >Wallet</q-toolbar-title
         >
-        <q-btn flat round dense icon="settings" @click="settingsClickHandler" />
-        <!-- <q-btn flat @click="drawer = !drawer" round dense icon="menu" /> -->
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="drawer" show-if-above :width="200" :breakpoint="400">
+    <q-drawer show-if-above :width="200" :breakpoint="400">
       <q-scroll-area
         style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd"
       >
-        <q-list padding>
-          <q-item clickable v-ripple @click="() => push('/dashboard')">
-            <q-item-section avatar>
-              <q-icon name="space_dashboard" />
-            </q-item-section>
-
-            <q-item-section>Dashboard</q-item-section>
-          </q-item>
-
-          <q-item clickable v-ripple @click="() => push('/')">
-            <q-item-section avatar>
-              <q-icon name="account_balance" />
-            </q-item-section>
-
-            <q-item-section>Accounts</q-item-section>
-          </q-item>
-
-          <q-item active clickable v-ripple @click="()=>push('/transactions')">
-            <q-item-section avatar>
-              <q-icon name="payment" />
-            </q-item-section>
-
-            <q-item-section>Transactions</q-item-section>
-          </q-item>
-
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon name="send" />
-            </q-item-section>
-
-            <q-item-section> Send </q-item-section>
-          </q-item>
-
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon name="settings" />
-            </q-item-section>
-
-            <q-item-section>Settings</q-item-section>
-          </q-item>
-        </q-list>
+        <SideMenu />
       </q-scroll-area>
 
       <q-img
@@ -79,40 +42,18 @@
       </q-page>
     </q-page-container>
   </q-layout>
-
-  <!-- <q-layout
-    view="lHh lpr lFf"
-    container
-    :style="{ height: `${height}px` }"
-    class="shadow-2 rounded-borders"
-  >
-    <q-header elevated>
-      <q-toolbar>
-        <WalletIcon class="cursor-pointer" @click="navigateHome" />
-        <q-toolbar-title class="non-selectable cursor-pointer" @click="navigateHome"
-          >Wallet</q-toolbar-title
-        >
-        <span>Time To Logout: {{ timeToLogout }} minutes</span>
-        <span style="padding-left: 10px">{{ userName }}</span>
-        <q-btn flat round dense icon="settings" @click="settingsClickHandler" />
-      </q-toolbar>
-    </q-header>
-
-    <q-page-container>
-      <q-page padding>
-        <RouterView />
-      </q-page>
-    </q-page-container>
-  </q-layout> -->
 </template>
 
 <script setup lang="ts">
 import { onBeforeMount, onMounted, ref, watch } from 'vue';
 import { RouterView, useRouter } from 'vue-router';
 import { debounce } from 'quasar';
-import WalletIcon from '@/components/icons/WalletIcon.vue';
 import { mgr } from '@/router/auth-service';
+import { useAuthGuard } from '@/composables';
+import SideMenu from '@/components/SideMenu.vue';
 const { push } = useRouter();
+
+const { state: authGuardState } = useAuthGuard();
 
 const height = ref<number>(window.innerHeight);
 const userName = ref<string>();
@@ -123,13 +64,11 @@ const resizeHandler = debounce(() => {
   height.value = window.innerHeight;
 }, 200);
 
-const settingsClickHandler = (): void => {
-  push('/settings');
-};
 
 const navigateHome = (): void => {
   push('/');
 };
+
 
 const countDownTimer = () => {
   timeToLogout.value = Math.round(userValue.value.expires_in / 60);
@@ -150,7 +89,7 @@ onMounted(async () => {
   userName.value = user?.profile.name;
 });
 
-onBeforeMount(() => {
+onBeforeMount(async () => {
   // TODO: Remove resize handler on component unmount
   window.addEventListener('resize', resizeHandler);
 });
---
+++ wallet-vue/src/components/SideMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-list padding>
+    <q-item
+      v-for="item in items"
+      v-bind:key="item.label"
+      :active="route.path === item.path"
+      clickable
+      v-ripple
+      @click="() => push(item.path)"
+    >
+      <q-item-section avatar>
+        <q-icon :name="item.iconName" />
+      </q-item-section>
+
+      <q-item-section>{{ item.label }}</q-item-section>
+    </q-item>
+  </q-list>
+</template>
+
+<script setup lang="ts">
+import { useSideMenu } from '@/composables';
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const { push } = useRouter();
+
+const { items } = useSideMenu();
+</script>
