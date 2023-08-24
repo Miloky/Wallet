eec07dd023316d679dd0392fa1e700322dbe39a6
miloky <monmorti@gmail.com>
2023-08-24 18:24:34 +0300

    pages

--- wallet-vue/src/router/index.ts
+++ wallet-vue/src/router/index.ts
@@ -1,15 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '@/pages/HomeView.vue';
-import LoginPage from '@/pages/LoginPage.vue';
-import CallBack from '@/pages/CallBack.vue';
+import DashboardPage from '@/pages/DashboardPage.vue';
+import AccountsPage from '@/pages/AccountsPage.vue';
+import TransactionsPage from '@/pages/TransactionsPage.vue';
+import LogoutPage from '@/pages/LogoutPage.vue';
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      name: 'Dashboard',
       path: '/',
-      name: 'home',
-      component: HomeView
+      component: DashboardPage
+    },
+    {
+      name: 'Accounts',
+      path: '/accounts',
+      component: AccountsPage
+    },
+    {
+      name: 'Transactions',
+      path: '/transactions',
+      component: TransactionsPage
     },
     {
       path: '/account/:id',
@@ -17,22 +29,17 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../pages/AccountDetails.vue')
+      component: () => import('../pages/AccountDetailsPage.vue')
     },
     {
-      path: '/callback',
-      name: 'Callback',
-      component: CallBack
+      name: 'Logout',
+      path: '/logout',
+      component: LogoutPage
     },
     {
-      path: '/login',
-      name: 'Login',
-      component: LoginPage
-    },
-    {
-      path: "/:catchAll(.*)",
+      path: '/:catchAll(.*)',
       name: '404',
-      component: () => import('../pages/NotFound44.vue')
+      component: () => import('../pages/NotFoundPage.vue')
     }
   ]
 });
