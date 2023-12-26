be92ed2f6ac6d774c24e5062deb373d0d02c8c80
Miloky <monmorti@gmail.com>
2023-12-26 20:38:58 +0200

    Test console

---
+++ Wallet.TestConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using Azure.Storage.Blobs;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+        Uri accountUri = new Uri("https://MYSTORAGEACCOUNT.blob.core.windows.net/");
+        BlobServiceClient client = new BlobServiceClient(accountUri, new DefaultAzureCredential());
+    }
+}
